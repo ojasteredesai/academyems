1c49d1102a79424fed03008ffe0bcfa92c1228db
ojas teredesai <ojasteredesai@gmail.com>
2024-03-16 20:28:18 +0530

    initial commit for ui and api projects

---
+++ academyems.mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace academyems.mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ academyems.mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace academyems.mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
