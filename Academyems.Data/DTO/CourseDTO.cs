b8b9e88439893a37e0962be3d896a72eacb77c1e
ojas teredesai <ojasteredesai@gmail.com>
2024-11-24 11:36:30 +0530

    removed mobile project from api project

--- Academyems.Data/DTO/CourseDTO.cs
+++ Academyems.Data/DTO/CourseDTO.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public int CourseTypeId { get; set; }
+        public string CourseTypeName { get; set; }
         public string CourseName { get; set; }
         public string? CourseDescription { get; set; }
     }
