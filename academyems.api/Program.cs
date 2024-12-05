146c8313850bf1b9c218c0e159b74f5a3686a195
ojas teredesai <ojasteredesai@gmail.com>
2024-12-05 22:17:37 +0530

    added cors

--- academyems.api/Program.cs
+++ academyems.api/Program.cs
@@ -9,6 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("GlobalConnectionStr")));
 
 builder.Services.AddControllers();
+
+var emsOrigins = "_emsOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: emsOrigins, policy =>
+    {
+        policy.WithOrigins("http://localhost:4200");
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,6 +26,10 @@ builder.Services.AddTransient<ICourseTypeRepository, CourseTypeRepository>();
 builder.Services.AddTransient<ICourseTypeService, CourseTypeService>();
 
 var app = builder.Build();
+
+
+app.UseCors(emsOrigins);
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
